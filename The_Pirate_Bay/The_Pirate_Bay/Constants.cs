4a9bba2ed30a053aa189489de28552d7757230bf
nagarjunRKottam <nagkottam@gmail.com>
2019-02-27 17:50:22 -0500

    Intial release
    Intial release

---
+++ The_Pirate_Bay/The_Pirate_Bay/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace The_Pirate_Bay
+{
+    public class Constants
+    {
+        public static string UrlSearch = "{0}/search/{1}/{2}/{3}/{4}";
+        public static string[] UrlTpb = new string[] { "http://pirateproxy.pl", "http://thepiratebay.sx"};
+    }
+}
